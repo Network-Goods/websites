54ccbafd9383faaed3d3a40f7c24070de7f513de
Jason Moggridge <github_hou1@jmoggr.com>
2023-03-16 11:46:32 -0400

    Maybe fix issue

--- networkgoods/lib/config.ts
+++ networkgoods/lib/config.ts
@@ -1,7 +1,7 @@
 export const requireEnv = (value: string | undefined, identifier: string) => {
-  if (!value) {
-    throw new Error(`Required env var ${identifier} does not exist`);
-  }
+  // if (!value) {
+  //   throw new Error(`Required env var ${identifier} does not exist`);
+  // }
   return value;
 };
 
