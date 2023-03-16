e639cd64dbee10f98a94eb4aa6f0a8b63d070b50
Jason Moggridge <github_hou1@jmoggr.com>
2023-03-16 11:48:36 -0400

    Debug vercel

--- networkgoods/lib/config.ts
+++ networkgoods/lib/config.ts
@@ -2,6 +2,7 @@ export const requireEnv = (value: string | undefined, identifier: string) => {
   // if (!value) {
   //   throw new Error(`Required env var ${identifier} does not exist`);
   // }
+  
   return value;
 };
 
