92cae1c80a693ac2e8219eeac9c8d74828a8d830
Jason Moggridge <github_hou1@jmoggr.com>
2023-05-16 11:42:28 -0400

    Test geofencing

--- fundingthecommons/pages/api/isGeofenced.ts
+++ fundingthecommons/pages/api/isGeofenced.ts
@@ -29,7 +29,7 @@ function getIsGeofenced(req: NextApiRequest): boolean {
   // Japan: JP
   // Thailand: TH
 
-  const blockedCountries = ['AU', 'HK', 'IN', 'ID', 'JP', 'TH'];
+  const blockedCountries = ['AU', 'HK', 'IN', 'ID', 'JP', 'TH', 'CA'];
 
   return blockedCountries.includes(countryCode);
 }
