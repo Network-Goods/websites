4b0d192c51e465f9240f96003595237b1712a58a
Jason Moggridge <github_hou1@jmoggr.com>
2023-05-16 11:12:30 -0400

    Add geofencing api to fundingthecommons

--- fundingthecommons/pages/api/isGeofenced.ts
+++ fundingthecommons/pages/api/isGeofenced.ts
@@ -1,24 +1,35 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  isGeofenced: boolean,
-  countryCode?: string | string[],
+  isGeofenced: boolean
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const keys = Object.keys(req.headers);
+  let isGeofenced = getIsGeofenced(req);
 
-  // Australia:
-  // Hong Kong: 
-  // India
-  // Indonesia: 
-  // Japan:
-  // Thailand: 
+  res.status(200).json({ isGeofenced })
+}
 
-  const countryCode = req.headers['x-vercel-ip-country'] || 'not set';
-  res.status(200).json({ isGeofenced: false, countryCode: keys })
+function getIsGeofenced(req: NextApiRequest): boolean {
+  const ipCountryHeader = req.headers['x-vercel-ip-country'];
+  if (!ipCountryHeader) {
+    return false;
+  }
+ 
+  const countryCode: string = Array.isArray(ipCountryHeader) ? ipCountryHeader[0] : ipCountryHeader;
+
+  // Blocked countries
+  // Australia: AU
+  // Hong Kong: HK 
+  // India: IN
+  // Indonesia: ID 
+  // Japan: JP
+  // Thailand: TH
+
+  const blockedCountries = ['AU', 'HK', 'IN', 'ID', 'JP', 'TH'];
+
+  return blockedCountries.includes(countryCode);
 }
